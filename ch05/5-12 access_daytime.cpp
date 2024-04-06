c6ed79148c1c9b63a17be840f8dfb3aac08e2ac2
uestc-zb <2459262592@qq.com>
2024-04-06 09:10:56 +0800

    finish ch05

---
+++ ch05/5-12 access_daytime.cpp
@@ -0,0 +1,39 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <assert.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    if(argc < 2){
+        printf("usage: %s ip_address port_number receive_buffer_size\n", basename(argv[0]));
+        return 1;
+    }
+    char *host = argv[1];
+    struct hostent* hostinfo = gethostbyname(host);
+    assert(hostinfo);
+    struct servent* servinfo = getservbyname("daytime", "tcp");
+    assert(servinfo);
+
+    printf("daytime port is %d\n", ntohs(servinfo->s_port));
+
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_port = servinfo->s_port;
+    address.sin_addr = *(struct in_addr*)hostinfo->h_addr_list;
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    int result = connect(sockfd, (struct sockaddr* )&address, sizeof(address));
+    assert(result != -1);
+
+    char buffer[128];
+    result = read(sockfd, buffer, sizeof(buffer));
+    assert(result > 0);
+    buffer[result] = '\0';
+    printf("the day tiem is :%s", buffer);
+    close(sockfd);
+    return 0;
+}
