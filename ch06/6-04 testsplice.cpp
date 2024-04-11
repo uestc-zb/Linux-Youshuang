c082e8aac9202c9f62aa13fee07128e776b4e45b
uestc-zb <2459262592@qq.com>
2024-04-11 12:36:49 +0800

    ch06 finish

--- ch06/6-04 testsplice.cpp
+++ ch06/6-04 testsplice.cpp
@@ -7,25 +7,16 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
-#include <sys/sendfile.h>
-
+#include <iostream>
 int main(int argc, char *argv[])
 {
-    if(argc <= 3){
-        printf("usage: %s ip_address port_number filename\n", basename(argv[0]));
+    if (argc < 3) {
+        printf("usage: %s ip_address port_number\n", basename(argv[0]));
         return 1;
     }
     const char* ip = argv[1];
     int port = atoi(argv[2]);
-    const char* file_name = argv[3];
-
-    int filefd = open(file_name, O_RDONLY);
-    assert(filefd > 0);
-    struct stat stat_buf;
-    fstat(filefd, &stat_buf);
 
     struct sockaddr_in address;
     bzero(&address, sizeof(address));
@@ -36,21 +27,28 @@ int main(int argc, char *argv[])
     int sock = socket(PF_INET, SOCK_STREAM, 0);
     assert(sock >= 0);
 
-    int ret = bind(sock, (struct sockaddr*)&address, sizeof(address));
+    int ret = bind(sock, (struct sockaddr* )&address, sizeof(address));
+    std::cout << errno << std::endl;
     assert(ret != -1);
 
     ret = listen(sock, 5);
     assert(ret != -1);
 
     struct sockaddr_in client;
-    socklen_t client_addrlength = sizeof(client);
+    socklen_t client_addrlength;
     int connfd = accept(sock, (struct sockaddr* )&client, &client_addrlength);
     if(connfd < 0) {
         printf("errno is: %d\n", errno);
     } else {
-        sendfile(connfd, filefd, NULL, stat_buf.st_size);
+        int pipefd[2];
+        assert(ret != -1);
+        ret = pipe(pipefd);
+        ret = splice(connfd, NULL, pipefd[1], NULL, 32768, SPLICE_F_MORE | SPLICE_F_MOVE);
+        assert(ret != -1);
+        ret = splice(pipefd[0], NULL, connfd, NULL, 32768, SPLICE_F_MORE | SPLICE_F_MOVE);
+        assert(ret != -1);
         close(connfd);
     }
-    close(connfd);
+    close(sock);
     return 0;
 }
